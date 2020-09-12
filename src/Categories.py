d1aa02215d01e971d80ee0d9256bbd21c91277d0
rahqis <rahqi@ad.unc.edu>
2020-09-12 11:37:10 -0400

    master

--- src/Categories.py
+++ src/Categories.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import importlib
-import Charts
 
 class Category:
     def __init__(self, name, cost):
@@ -16,54 +14,54 @@ def runSide():
     st.sidebar.header("Income")
 
     incomeStr = 'Estimated Income'
-    income = st.sidebar.number_input(incomeStr)
+    income = st.sidebar.number_input(incomeStr, value=0.00)
     inc = Category(incomeStr, income)
 
     st.sidebar.header("Estimated expenses")
 
 
     transportationStr = 'Transportation'
-    transportation = st.sidebar.number_input(transportationStr)
+    transportation = st.sidebar.number_input(transportationStr, value=0.00)
     trans = Category(transportationStr, transportation)
     catergories.append(trans)
 
     homeStr = 'Home & Utilities'
-    home = st.sidebar.number_input(homeStr)
+    home = st.sidebar.number_input(homeStr, value=0.00)
     homeutil = Category(homeStr, home)
     catergories.append(homeutil)
 
     groceriesStr = 'Groceries'
-    groceries = st.sidebar.number_input(groceriesStr)
+    groceries = st.sidebar.number_input(groceriesStr, value=0.00)
     groc = Category(groceriesStr, groceries)
     catergories.append(groc)
 
     personalStr = 'Personal & Family Care'
-    personal  = st.sidebar.number_input(personalStr)
+    personal  = st.sidebar.number_input(personalStr, value=0.00)
     pers = Category(personalStr, personal)
     catergories.append(pers)
 
     healthStr = 'Health'
-    health = st.sidebar.number_input(healthStr)
+    health = st.sidebar.number_input(healthStr, value=0.00)
     heal = Category(healthStr, health)
     catergories.append(heal)
 
     diningStr = 'Restaurants & Dining'
-    dining = st.sidebar.number_input(diningStr)
+    dining = st.sidebar.number_input(diningStr, value=0.00)
     dine = Category(diningStr, dining)
     catergories.append(dine)
 
     shoppingStr = 'Shopping & Entertainment'
-    shopping = st.sidebar.number_input(shoppingStr)
+    shopping = st.sidebar.number_input(shoppingStr, value=0.00)
     shop = Category(shoppingStr, shopping)
     catergories.append(shop)
 
     savingsStr = 'Savings & Investments'
-    savings = st.sidebar.number_input(savingsStr)
+    savings = st.sidebar.number_input(savingsStr, value=0.00)
     save = Category(savingsStr, savings)
     catergories.append(save)
 
     otherStr = 'Other and Miscellaneous'
-    other = st.sidebar.number_input(otherStr)
+    other = st.sidebar.number_input(otherStr, value=0.00)
     misc = Category(otherStr, other)
     catergories.append(misc)
     
@@ -89,5 +87,33 @@ def generatePieSpend():
     st.header('Your Spending Chart')
     st.pyplot()
 
+
+    
+def highExepense():
+    highestExpense = catergories[0]
+    highest2Expense = catergories[1]
+    highest3Expense = catergories[2]
+
+    for c in catergories:
+        if(highestExpense.cost<c.cost):
+            highestExpense = c
+        if(highest2Expense == highestExpense):
+            highest2Expense = catergories[0]
+    for c in catergories:
+        if(highest2Expense.cost<highestExpense.cost and highest2Expense.cost<c.cost and c != highestExpense):
+            highest2Expense = c
+    for c in catergories:
+        if(highest3Expense.cost<=highest2Expense.cost and highest3Expense.cost<c.cost and (c != highest2Expense and c != highestExpense)):
+            highest3Expense = c
+    
+    highestExpenses = [highestExpense, highest2Expense, highest3Expense]
+    
+    st.subheader('Top 3 Expenses')
+    for h  in highestExpenses:
+        st.write(h.name)
+        st.write('{:06.2f}'.format(h.cost))
+
 if st.sidebar.button("View Spending Chart"):
     generatePieSpend()
+    highExepense()    
+
